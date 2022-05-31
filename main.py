4c9dc498e0af48ffd05a45463fa3c3d038227acb
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-05-31 12:07:28 +0500

    Updated Main.js!

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 # Importing libraries
 import pandas as pd
-import numpy as np
 
 # Read csv file into a pandas dataframe
 df = pd.read_csv("property data.csv")
 
 # Take a look at the first few rows
 print("Real CSV Data: \n", df.head(), 'asd', df.columns.to_list())
-# print(df.head(), 'asd', df.columns.to_list())
 
 # Making a list of missing value types
 missing_values = ["n/a", "na", "--", " ", ""]
@@ -16,49 +14,3 @@ df = pd.read_csv("property data.csv", na_values=missing_values)
 all_columns = df.columns.to_list()
 
 df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=True)
-
-print(df.columns.tolist(), 'asdasdas')
-# Looking at the NUM_BEDROOMS column
-# print(df['NUM_BEDROOMS'])
-# print(df['NUM_BEDROOMS'].isnull())
-
-
-# # Looking at the OWN_OCCUPIED column
-# print(df['OWN_OCCUPIED'])
-# print(df['OWN_OCCUPIED'].isnull())
-
-
-# # Detecting numbers
-# cnt = 0
-# for row in df['OWN_OCCUPIED']:
-#     try:
-#         int(row)
-#         df.loc[cnt, 'OWN_OCCUPIED'] = np.nan
-#     except ValueError:
-#         pass
-#     cnt += 1
-
-
-# # Total missing values for each feature
-# print(df.isnull().sum())
-
-
-# # Any missing values?
-# print(df.isnull().values.any())
-
-
-# # Total number of missing values
-# print(df.isnull().sum().sum())
-
-
-# # Replace missing values with a number
-# df['ST_NUM'].fillna(125, inplace=True)
-
-
-# # Location based replacement
-# df.loc[2, 'ST_NUM'] = 125
-
-
-# # Replace using median
-# median = df['NUM_BEDROOMS'].median()
-# df['NUM_BEDROOMS'].fillna(median, inplace=True)
